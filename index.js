ab4602ef6e201d9a6a07c677ed0f37f1f11b86c9
Tyler Corbett <thetylercorbett@gmail.com>
2018-10-23 16:18:42 -0700

    winning number is 7. can check if input is correct or not

--- index.js
+++ index.js
@@ -1,38 +1,17 @@
 var randomNumberForm = document.getElementById('random-number-form');
 
-console.log(randomNumberForm)
+var guessCounter = 0;
 
 
-// Submit button
-function checkRandomNumber() {
-    var elements = randomNumberForm.elements;
-    var randomNumberInput = elements.randomNumberText;
-    var numberGuessed = randomNumberInput.value;
-}
-
-// Reset button
-function resetRandomNumber() {
-    console.log('Reset button works')
-}
-
-
-
-
-
-
-
-
-
-//This program generates a random number between 1 and 10, you have 3 guesses to figure out the number
-
+    
 /*
 
-START GAME
-Generate a random number
-var guessCount = 0;
+START GAME      DONE
+Generate a random number DONE
+var guessCount = 0; DONE
 
 GAMEPLAY LOOP
-while (guessCount > 0):
+while (guessCount > 0): DONE
     Prompt user for input
     Check if user input is between 1 & 10
     Check if input === randomNumber
@@ -49,14 +28,34 @@ while (guessCount > 0):
 USER LOSES
 if (guessCount === 0):
     print(you lose)
-
-
-
-
-
-
-
-
-
-
 */
+    
+
+
+// Submit button
+function checkRandomNumber() {
+    var elements = randomNumberForm.elements;
+    var randomNumberInput = elements.randomNumberText;
+    var numberGuessed = randomNumberInput.value;
+    var secretNumber = 7;
+
+    if (numberGuessed == secretNumber) {
+        console.log('You win!')
+    }
+    else {
+        console.log('Wrong')
+    }
+
+}
+
+// Reset button
+function resetRandomNumber() {
+    console.log('Reset button works')
+}
+
+
+
+
+
+
+
