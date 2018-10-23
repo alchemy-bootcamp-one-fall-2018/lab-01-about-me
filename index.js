1f9e2394ae7502232116cffe36a5097ea16b1115
Tyler Corbett <thetylercorbett@gmail.com>
2018-10-23 16:48:44 -0700

    game now functions up. has gameover screen. counts down number of guesses left.

--- index.js
+++ index.js
@@ -1,36 +1,9 @@
 var randomNumberForm = document.getElementById('random-number-form');
+var randomNumberResponse = document.getElementById('random-number-response');
 
-var guessCounter = 0;
 
 
-    
-/*
-
-START GAME      DONE
-Generate a random number DONE
-var guessCount = 0; DONE
-
-GAMEPLAY LOOP
-while (guessCount > 0): DONE
-    Prompt user for input
-    Check if user input is between 1 & 10
-    Check if input === randomNumber
-    if (correct number):
-        print(you win)
-    else:
-        if (guess > randomNumber):
-            print(too high)
-            add 1 to guessCount
-        else:
-            print(too low)
-            add 1 to guessCount
-
-USER LOSES
-if (guessCount === 0):
-    print(you lose)
-*/
-    
-
+var guessCounter = 3;
 
 // Submit button
 function checkRandomNumber() {
@@ -38,19 +11,28 @@ function checkRandomNumber() {
     var randomNumberInput = elements.randomNumberText;
     var numberGuessed = randomNumberInput.value;
     var secretNumber = 7;
+    var guessButton = elements.guess;
 
     if (numberGuessed == secretNumber) {
-        console.log('You win!')
+        randomNumberResponse.textContent = 'You guessed correctly!'
     }
     else {
-        console.log('Wrong')
+        if (guessCounter > 0) {
+        randomNumberResponse.textContent = 'Incorrect. You have ' + guessCounter + ' guesses left.'
+        guessCounter -= 1;
+        }
+        else {
+            randomNumberResponse.textContent = 'Game over!'
+            guessButton.disabled = true;
+        }
     }
 
 }
 
 // Reset button
 function resetRandomNumber() {
-    console.log('Reset button works')
+    var elements = randomNumberForm.elements;
+
 }
 
 
