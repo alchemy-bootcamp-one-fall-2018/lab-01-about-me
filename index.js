2251685d76e556d57c744a4a299266b2436c5853
Tyler Corbett <thetylercorbett@gmail.com>
2018-10-23 15:29:10 -0700

    submit and reset buttons are linked to js

---
+++ index.js
@@ -0,0 +1,53 @@
+function checkRandomNumber() {
+    console.log('Sunmit button works')
+}
+
+function resetRandomNumber() {
+    console.log('Reset button works')
+}
+
+
+
+
+
+
+
+
+
+//This program generates a random number between 1 and 10, you have 3 guesses to figure out the number
+
+/*
+
+START GAME
+Generate a random number
+var guessCount = 0;
+
+GAMEPLAY LOOP
+while (guessCount > 0):
+    Prompt user for input
+    Check if user input is between 1 & 10
+    Check if input === randomNumber
+    if (correct number):
+        print(you win)
+    else:
+        if (guess > randomNumber):
+            print(too high)
+            add 1 to guessCount
+        else:
+            print(too low)
+            add 1 to guessCount
+
+USER LOSES
+if (guessCount === 0):
+    print(you lose)
+
+
+
+
+
+
+
+
+
+
+*/
