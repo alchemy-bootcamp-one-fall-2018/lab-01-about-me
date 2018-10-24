326a495d477b8903a32d8777df8ece069ba8f24f
Dyanna Tolman <dyannast@gmail.com>
2018-10-23 17:44:08 -0700

    guess-number game works

--- guess-number.js
+++ guess-number.js
@@ -1,3 +1,5 @@
+/* exported checkGuessNumber, resetGuessNumber */
+
 var guessNumber = document.getElementById('guess-number');
 var guessNumberResponse = document.getElementById('guess-number-response');
 
@@ -6,18 +8,30 @@ var guessCount = 0;
 function checkGuessNumber() {
 
     var elements = guessNumber.elements;
-
     var guess = elements.number.value;
     var submit = elements.guess;
 
-    if(guess === 7) {
-        guessNumberResponse.textContent = "You got it. You win!";
+    if(guess === '7') {
+        guessNumberResponse.textContent = 'You got it. You win!';
         submit.disabled = true;
     }
     else {
-        
+        guessCount = guessCount + 1;
+
+        if(guessCount < 3) {
+            guessNumberResponse.textContent = 'try again!';
+        }
+        else {
+            guessNumberResponse.textContent = 'you lose!';
+            submit.disabled = true;
+        }
     }
+}
 
-
-
+function resetGuessNumber() {
+    var elements = guessNumber.elements;
+    var submit = elements.guess;
+    submit.disabled = false;
+    guessCount = 0;
+    guessNumberResponse.textContent = '';
 }
