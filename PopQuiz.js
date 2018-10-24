9e72a22ba7d8c09a48b0992e1737ce8aac83b635
Cheri A Portman <cheriportman@hotmail.com>
2018-10-23 17:50:55 -0700

    game one in progress

---
+++ PopQuiz.js
@@ -0,0 +1,49 @@
+/* exported checkAboutMe, quizForm */
+
+var quizForm = document.getElementById('quiz');
+var quizResponse = document.getElementById('quiz-response');
+
+var correct = 0;
+
+function checkAboutMe() {
+    var elements = quizForm.elements;
+
+    var chocMeltInput = elements.melt;
+    var chocMelt = chocMeltInput.value; 
+
+    var petInput = elements.pet;
+    var favPet = petInput.value;
+
+    var fiberInput = elements.fiber;
+    var favFiber = fiberInput.value;
+
+    var gameInput = elements.game;
+    var likeGame = gameInput.value;
+
+    var junkieInput = elements.junkie;
+    var kindaJunkie = junkieInput.value;
+
+
+    if(chocMelt === 'tongue'){
+        correct++;
+    }   
+    if(favPet === 'dog'){
+        correct++;
+    }
+    if(favFiber === 'yarn'){
+        correct++;
+    }
+    if(likeGame === 'puzzle') {
+        correct++;
+    }
+    if(kindaJunkie === 'books') {
+        correct++;
+    }
+    var score = (correct / 5) * 100;
+    quizResponse.textContent = 'You scored ' + score + 'percent';
+}
+
+function resetAboutMe() {
+    correct = 0;
+    quizResponse.textContent = '';
+}
