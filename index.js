a5eef39465b5c2a1226c0512e098df8c62796912
Lance Owen Merrill <Lanceom@hawaii.edu>
2018-10-23 17:08:14 -0700

    lab-02 with 1 working game

--- index.js
+++ index.js
@@ -1,2 +1,58 @@
 
 
+
+var aboutMeForm= document.getElementById('about-me-game');
+var aboutMeAnswerCounter = document.getElementById('about-me-output');
+
+function checkAboutMe() {
+var aboutMeForm= document.getElementById('about-me-game');
+var elements = aboutMeForm.elements;
+var questionOneInput = elements.questionOneName.value.toLowerCase();
+
+var aboutMeForm= document.getElementById('about-me-game');
+var elements = aboutMeForm.elements;
+var questionTwoInput = elements.questionTwoName.value.toLowerCase();
+
+var aboutMeForm= document.getElementById('about-me-game');
+var elements = aboutMeForm.elements;
+var questionThreeInput = elements.questionThreeName.value.toLowerCase();
+
+var aboutMeForm= document.getElementById('about-me-game');
+var elements = aboutMeForm.elements;
+var questionFourInput = elements.questionFourName.value.toLowerCase();
+
+var aboutMeForm= document.getElementById('about-me-game');
+var elements = aboutMeForm.elements;
+var questionFiveInput = elements.questionFiveName.value.toLowerCase();
+
+
+
+var counter = 0
+if(questionOneInput === 'cycling'){
+counter = counter +1;
+};
+
+if(questionTwoInput==='bjj'){
+    counter = counter +1;
+};
+
+if(questionThreeInput==='acl'){
+    counter = counter +1;
+};
+
+if(questionFourInput==='smart contracts'){
+    counter = counter +1;
+};
+
+if(questionFiveInput==='lance'){
+    counter = counter +1;
+};
+
+aboutMeAnswerCounter.textContent= 'you have '+ counter + ' correct!';
+
+console.log('test', questionOneInput, questionTwoInput, counter)
+
+};
+
+
+function resetAboutMe() {};
