61192ea401e75c2fbc8fe3eb962e1f218af195b7
Teonna Zaragoza <teonna.zaragoza@gmail.com>
2018-10-23 21:57:55 -0700

    started game 2 in html with quesion and added game 2 contect in js. Added guess-number.js

---
+++ game-two.js
@@ -0,0 +1,25 @@
+var gameTwoForm = document.getElementById('game-two-form');
+var gameTwoResponse = document.getElementById('game-two-response');
+
+function checkGameTwo () {
+
+    var elements = gameTwoForm.elements;
+
+    var gearInput = elements.gear;
+    var gearPref = gearInput.value;
+
+    var correct = 0;
+
+
+    if(gearPref === 'Nike') {
+        correct = correct + 1;
+        gearInput.classList.add('correct');
+        gearInput.classList.remove('incorrect');
+
+    }else if(gearPref !== 'Nike'){
+        gearInput.classList.add('incorrect');
+        gearInput.classList.remove('correct');
+    }
+
+    gameTwoResponse.textContent = 'You got ' + correct + ' /5 right!';
+}
