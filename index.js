7dede88c13a015a552b4927dba060f3e0e846d0d
Lance Owen Merrill <Lanceom@hawaii.edu>
2018-10-24 08:55:14 -0700

    updated with number based guessing game- Second game works and prints properly

--- index.js
+++ index.js
@@ -7,22 +7,22 @@ function checkAboutMe() {
     var aboutMeForm = document.getElementById('about-me-game');
     var elements = aboutMeForm.elements;
     var questionOneInput = elements.questionOneName.value.toLowerCase();
-    
-    var aboutMeForm = document.getElementById('about-me-game');
-    var elements = aboutMeForm.elements;
-    var questionTwoInput = elements.questionTwoName.value.toLowerCase();
 
-    var aboutMeForm = document.getElementById('about-me-game');
-    var elements = aboutMeForm.elements;
-    var questionThreeInput = elements.questionThreeName.value.toLowerCase();
+    var aboutMeForm2 = document.getElementById('about-me-game');
+    var elements2 = aboutMeForm2.elements;
+    var questionTwoInput = elements2.questionTwoName.value.toLowerCase();
 
-    var aboutMeForm = document.getElementById('about-me-game');
-    var elements = aboutMeForm.elements;
-    var questionFourInput = elements.questionFourName.value.toLowerCase();
+    var aboutMeForm3 = document.getElementById('about-me-game');
+    var elements3 = aboutMeForm3.elements;
+    var questionThreeInput = elements3.questionThreeName.value.toLowerCase();
 
-    var aboutMeForm = document.getElementById('about-me-game');
-    var elements = aboutMeForm.elements;
-    var questionFiveInput = elements.questionFiveName.value.toLowerCase();
+    var aboutMeForm4 = document.getElementById('about-me-game');
+    var elements4 = aboutMeForm4.elements;
+    var questionFourInput = elements4.questionFourName.value.toLowerCase();
+
+    var aboutMeForm5 = document.getElementById('about-me-game');
+    var elements5 = aboutMeForm5.elements;
+    var questionFiveInput = elements5.questionFiveName.value.toLowerCase();
 
 
 
@@ -54,22 +54,22 @@ function checkAboutMe() {
 
 var guessingGameForm = document.getElementById('guessingGame');
 var guessingGameOutput = document.getElementById('guessingGameOutput');
-var guessingGameCounter = 0;
+var guessingGameCounter = 5;
 
 function checkGuessingGame() {
-
-    var randomNumber = Math.floor(Math.random() * 5);
+    
     var elements = guessingGameForm.elements;
     var userGuess = elements.guessNumber.value;
 
-    console.log(userGuess);
-    if (userGuess == randomNumber){
+    if(userGuess == 3){
         guessingGameOutput.textContent = 'Correct!';
-        guessingGameCounter = 0;
+    
     }
     else {
-        guessingGameCounter = guessingGameCounter + 1;
-        guessingGameOutput.textContent = 'Wrong, you have ' + guessingGameCounter + 'guesses left';
+        guessingGameCounter = guessingGameCounter - 1;
+        if(guessingGameCounter < 4){
+            guessingGameOutput.textContent = 'Wrong, you have ' + guessingGameCounter + ' guesses left';
+        }
             
     }
         
