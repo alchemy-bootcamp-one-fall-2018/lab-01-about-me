302e5941080eb1806a9f5af7a63e2a340d97b72d
Jennifer_Ramirez <jenramirez5@hotmail.com>
2018-10-23 17:58:07 -0700

    started js work

---
+++ game.js
@@ -0,0 +1,15 @@
+var aboutMeForm = document.getElementById("about-meform");
+var aboutMeResponse = document.getElementById("about-me-response");
+
+function checkAboutMe() {
+    var elements = aboutMeForm.elements;
+
+    var favoriteInput = elements.favorite;
+    var likeClassmates = favoriteInput.value.toLowerCase();
+
+}
+
+function submit {
+    var newVar = document.getElementById("submitCheckbox");
+}
+console.log ("Help me");
