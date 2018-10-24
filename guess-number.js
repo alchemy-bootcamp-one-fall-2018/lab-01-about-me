529a81a305de47ad4d9af0625739bb77e85e5394
Teonna Zaragoza <teonna.zaragoza@gmail.com>
2018-10-23 22:06:20 -0700

    added guess number counter to js file. It still doesn't work.

---
+++ guess-number.js
@@ -0,0 +1,34 @@
+var guessNumberForm = document.getElementById('guess-number-form');
+var guessNumberResponse = document.getElementById('guess-number-response');
+
+var guessCount = 0;
+
+function checkGuessNumber() {
+
+    var elements = guessNumberForm.elements;
+    var guess = elements.number.value;
+    var submit - elements.guess;
+
+    //esLint-disable-next-line eqeqeq
+    if(guess ==4) {
+        guessNumberResponse.textContect = 'You Win!';
+        submit.disabled = true;
+    
+    }
+    else {
+        guessCount = guessCount + 1;
+
+        if(guessCount < 4) {
+            guessNumberResponse.textContent = 'You lose';
+            submit.disabled = true;
+        }
+    }
+
+}
+
+function resetGuessNumber() {
+    var elements = guessNumberForm.elements;
+    var submit = elements.guess;
+    guessCount = 0;
+    guessNumberResponse.textContent = '';
+}
