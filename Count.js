f694cf28437ed0b9bb987b3fd3023db9c174e0ce
AdamTHardy <adam.t.hardy@me.com>
2018-10-24 16:58:11 -0700

    midway commit console working

--- Count.js
+++ Count.js
@@ -7,6 +7,24 @@ function tryQuerySelector() {
 
     for(var i = 0; i < elementInputs.length; i++) {
         var input = elementInputs[i];
-        console.log(input.value, input.id, input.checked);
+        console.log(input.value, input.checked);
+    }
+
+    var section = document.querySelectorAll('section');
+
+    for(var j = 0; j < section.length; j++) {
+        console.log(section.length);
+    }
+
+    var form = document.querySelectorAll('form');
+
+    for(var k = 0; k < form.length; k++) {
+        console.log(form.length);
+    }
+
+    var div = document.querySelectorAll('div');
+
+    for(var l = 0; l < div.length; l++) {
+        console.log(div.length);
     }
 }
