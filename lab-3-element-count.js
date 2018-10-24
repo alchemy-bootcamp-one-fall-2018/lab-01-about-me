86d6919ad9b5ca110bef6b92c5b3b1787e3b351d
Christina T. Herlocker <cherlocker3113@gmail.com>
2018-10-24 14:42:35 -0700

    checkboxes installed

--- lab-3-element-count.js
+++ lab-3-element-count.js
@@ -1,1 +1,7 @@
-@@@``
+/* Create checkboxes and a button
+Start by creating a series of checkboxes that all have the same name and each one has a value indicating what tag name in represents (p, section, input, etc). Try and include a broad set of tag names. Each checkbox should also have an associated label to visually show the user the corresponding tag name)
+
+Create a function in a stand alone .js file and log a message to let you know the function fired
+
+Wire the onclick of the button to call the function and validate that it logs the message */
+
