633a8209a31943dd384e431457c69665363905fc
Teonna Zaragoza <teonna.zaragoza@gmail.com>
2018-10-24 15:02:31 -0700

    added querySelector for checked boxes

--- elementCounter.js
+++ elementCounter.js
@@ -1,1 +1,13 @@
-  
+  function tryQuerySelector() {
+      var inputCounter = document.querySelectorAll('input[name="counter"');
+    
+      for(var i=0; i < inputCounter.length; i++) {
+          var input = inputCounter[i];
+          console.log(input.value, input.id, input.checked);
+      }
+
+      var selectedCounterInput = document.querySelector( 'input[name="counter"]:checked');
+
+    var inputCounter = document.querySelectorAll('')
+    }
+
