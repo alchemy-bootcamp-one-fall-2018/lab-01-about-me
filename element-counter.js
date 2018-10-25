8b17987ac197ee1f24feffef6b7d6a0ad080170b
Deborah Hardy <deb_hardy@me.com>
2018-10-24 18:36:12 -0700

    simplified for loop and added response for element counter

--- element-counter.js
+++ element-counter.js
@@ -4,28 +4,21 @@
 
 function tryQuerySelector() {
     var selectedElementInput = document.querySelectorAll('input[name="element"]:checked');
+    console.log(selectedElementInput);
     
     for(var i = 0; i < selectedElementInput.length; i++) {
         var input = selectedElementInput[i];
         console.log(input.value);
+        var tag = document.querySelectorAll(input.value);
+        console.log(tag.length);
+        
+        if(input.value === 'p') {
+            document.getElementById('p-length').innerHTML = input.value + ' - ' + tag.length;
+        }
+        else if(input.value === 'div') {
+            document.getElementById('div-length').innerHTML = input.value + ' - ' + tag.length;
+        }
+        else if(input.value === 'h1') {
+            document.getElementById('h1-length').innerHTML = input.value + ' - ' + tag.length;
+        }
     }
-
-    var p = document.querySelectorAll('p');
-
-    for(var j = 0; j < p.length; j++) {
-        console.log(p.length);
-    }
-
-    var div = document.querySelectorAll('div');
-
-    for(var k = 0; k < div.length; k++) {
-        console.log(div.length);
-    }
-
-    var h1 = document.querySelectorAll('h1');
-
-    for(var q = 0; q < h1.length; q++) {
-        console.log(h1.length);
-    }
-
-}
