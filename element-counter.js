480af1834e014e0207fb5281e5c5183e19212c54
Carmen V Ramos <carmenvramos@yahoo.com>
2018-10-24 17:34:08 -0700

    Added test if no tags selected and display appropriate message

--- element-counter.js
+++ element-counter.js
@@ -10,18 +10,18 @@ function tryQuerySelector() {
     var elementCounterResponse = document.getElementById('element-counter-response');
     var selectedTagInputs = document.querySelectorAll('input[name="tag"]:checked');
 
-    for(var i = 0; i < selectedTagInputs.length; i++) {
-        var input = document.querySelectorAll(selectedTagInputs[i].value);
-        console.log(selectedTagInputs[i].id, input.length);
-        elementCounterResponse.textContent = 'Element  ' + selectedTagInputs[i].id + ' is used ' + input.length + ' times.' ;
+    if(selectedTagInputs.length > 0) {
+        for(var i = 0; i < selectedTagInputs.length; i++) {
+            var input = document.querySelectorAll(selectedTagInputs[i].value);
+            console.log(selectedTagInputs[i].id, input.length);
+            elementCounterResponse.textContent = 'Element  ' + selectedTagInputs[i].id + ' is used ' + input.length + ' times.' ;
+        }
+       
     }
-
-    // if(selectedTagInput) {
-    //     console.log(selectedTagInput.value, selectedTagInput.length);
-    // }
-    // else {
-    //     console.log('no tag selected');
-    // }
+    else {
+        console.log('No tag selected');
+        elementCounterResponse.textContent = 'No tag selected';
+    }
 
    
 }
