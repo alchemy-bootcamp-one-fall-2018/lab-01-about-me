fe7797300411550c44a4a04a78177067dd82a38d
Christina T. Herlocker <cherlocker3113@gmail.com>
2018-10-24 17:44:28 -0700

    final commit

--- lab-3-element-count.js
+++ lab-3-element-count.js
@@ -1,7 +1,24 @@
-/* Create checkboxes and a button
-Start by creating a series of checkboxes that all have the same name and each one has a value indicating what tag name in represents (p, section, input, etc). Try and include a broad set of tag names. Each checkbox should also have an associated label to visually show the user the corresponding tag name)
 
-Create a function in a stand alone .js file and log a message to let you know the function fired
+/* exported countPElements */
+/* exported countSectionElements */
+/* exported countInputElements */
 
-Wire the onclick of the button to call the function and validate that it logs the message */
+function countPElements() {
+    var tags = document.querySelectorAll('p');
+    var countP = tags.length;
+    console.log(tags.length);
+    countPResponse.textContent = 'P:  ' + countP;
+}
 
+function countSectionElements() {
+    var tags = document.querySelectorAll('section');
+    var countSection = tags.length;
+    console.log(tags.length);
+    countSectionResponse.textContent = 'Section: ' + countSection;
+}
+function countInputElements() {
+    var tags = document.querySelectorAll('input');
+    var countInput = tags.length;
+    console.log(tags.length);
+    countInputResponse.textContent = 'Input: ' + countInput;
+}
