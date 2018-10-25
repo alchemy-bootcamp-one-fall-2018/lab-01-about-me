312a8a025490876330a4107da5895fe6c646241b
Dyanna Tolman <dyannast@gmail.com>
2018-10-24 22:05:21 -0700

    logging tag and count

--- element-count.js
+++ element-count.js
@@ -5,15 +5,10 @@
 function tryQuerySelector() {
     var selectedElements = document.querySelectorAll('input[name="elements"]:checked');
     console.log(selectedElements);
+
     for(var i = 0; i < selectedElements.length; i++) {
-        var input = selectedElements[i];
-        console.log(input.value);
-    
-        var elementCount = document.querySelectorAll('input.value');
-        console.log(elementCount.length);
+        var input = selectedElements[i].value;
+        console.log(document.querySelectorAll(input).length);
+               
     }
 }
-
-
-// var p = document.querySelectorAll('p');
-// console.log(p.length);
