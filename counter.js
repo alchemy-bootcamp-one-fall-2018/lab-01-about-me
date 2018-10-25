10ccbdc8bf409736a613d98279101faaa224c6ae
Tyler Corbett <thetylercorbett@gmail.com>
2018-10-24 17:49:19 -0700

    everything works! :)

--- counter.js
+++ counter.js
@@ -1,16 +1,30 @@
 var elementCounterResponse = document.getElementById('element-counter-response');
+var elementCounterResponseP = document.getElementById('element-counter-response-p');
+var elementCounterResponseInput = document.getElementById('element-counter-response-input');
 
 
 function clickedButton() {
     var tagsChosen = document.querySelectorAll('input[name="element-button"]:checked');    
     
-    for (var i = 0; i < tagsChosen.length; i++) {
+    elementCounterResponse.textContent = '';
+    elementCounterResponseP.textContent = '';
+    elementCounterResponseInput.textContent = '';
+
+    for(var i = 0; i < tagsChosen.length; i++) {
         var tagCount = tagsChosen[i];
         var divList = document.querySelectorAll(tagCount.value);
         var numberDivs = divList.length;
-
-        var finalAnswer = (tagCount.value + '--' + numberDivs)
-        console.log(finalAnswer);
-        elementCounterResponse.textContent = finalAnswer;
+        var finalAnswer = (tagCount.value + '--' + numberDivs);
+       
+        if(tagCount.value === 'div') {
+            elementCounterResponse.textContent = finalAnswer;
+        }
+        if(tagCount.value === 'p') {
+            elementCounterResponseP.textContent = finalAnswer;
+        }
+        if(tagCount.value === 'input') {
+            elementCounterResponseInput.textContent = finalAnswer;
+        }
+        
     }
 }
