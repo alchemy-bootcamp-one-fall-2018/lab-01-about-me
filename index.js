65b8a162c06efdb3aa2b51ee473d2fb7820182da
Connor Frendt <cjacksonfx@yahoo.com>
2018-10-25 08:47:47 -0700

    Working on trying to output the number of tags in html

--- index.js
+++ index.js
@@ -4,27 +4,31 @@
 
 
 function tryQuerySelector() {
-    var buttonInputs = document.querySelectorAll('input[name="button"]');
+    var buttonInputs = document.querySelectorAll('input[name="button"]:checked');
 
     for(var i = 0; i < buttonInputs.length; i++) {
         var input = buttonInputs[i];
         console.log(input.value, input.id, input.checked);
+        // console.log(buttonInputs[i].id, buttonInputs.length);
     }
-    var selectedButtonInput = document.querySelector('input[name="button"]:checked');
 
+    var selectedButtonInput = document.querySelector('input[name="button"]:checked');
     if(selectedButtonInput) {
-        console.log('Hey', selectedButtonInput.value);
+        console.log('Hey this is the tag', selectedButtonInput.value);
     } else {
         console.log('No Element Selected');
     }
-
+    
     var elements = ['p', 'h2', 'input'];
     for(var j = 0; j < elements.length; j++) {
         console.log(elements[j]);
     }
-
-    // var tags = document.querySelectorAll('input[name="tags"]:checked');
-    // for(var k; k < tags.length; k++) {
-    //     console.log(tags[k].value);
-    // }
+    
+    var tags = document.querySelectorAll('input[name="button"]:checked');
+    for(var k = 0; k < tags.length; k++) {
+        var tag = tags[k].value;
+        console.log(tags[k].length);
+        console.log(tag.length);
+    }
 }
+
