e9bbbcc3019aff8301b100d719449c95bcf68396
Shaba9 <shaba92@gmail.com>
2018-10-24 17:34:14 -0700

    stretch

--- analyze.js
+++ analyze.js
@@ -1,9 +1,10 @@
 'use strict';
-/* exported howMany */
+/* exported howMany, stretch */
+
 
 function howMany() {
-    var answer = document.getElementById('answer');
     var tags = document.querySelectorAll('input[name="searchElms"]:checked');
+    var answer = document.getElementById('answer');
     var msg = '';
 
     for(var i = 0; i < tags.length; i++){
@@ -13,4 +14,27 @@ function howMany() {
     }
     msg += 'Checked boxes - ' + tags.length + '<br>';
     answer.innerHTML = msg;
+
+    stretch(tags);
+}
+
+function stretch(tags) {
+    var tagCount = {};
+    var html = '<ul>';
+    var stretch = document.getElementById('stretch');
+    
+    for(var i = 0; i < tags.length; i++) {
+        var el = tags[i].value;
+        var elLength = document.querySelectorAll(el).length;
+        
+        tagCount[el] = elLength;
+    }
+    var keys = Object.keys(tagCount);
+    
+    for(var j = 0; j < tags.length; j++){
+        html += '<li>' + keys[j] + tagCount[keys[j]] + '</li>';
+    }
+
+    html += '</ul>';
+    stretch.innerHTML = html;
 }
