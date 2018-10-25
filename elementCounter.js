4b58822e1760cd7731ce612b352a7dffe25d4b00
Jennifer_Ramirez <jenramirez5@hotmail.com>
2018-10-24 18:23:15 -0700

    added java content

--- elementCounter.js
+++ elementCounter.js
@@ -1,17 +1,32 @@
 /* eslint no-console: "off" */
 /* exported tryQuerySelector*/
 
+'use strict';
+var elementResponseP = document.querySelectorAll('element-response-P');
+var elemenResponseDiv = document.querySelectorAll('element-response-Div');
+var elemenResponseLi = document.querySelectorAll('element-response-Li');
+var elemenResponseInput = document.querySelectorAll('element-response-Input');
+
 function tryQuerySelector() {
-    var elementInputs = document.querySelectorAll('input[name="element"]');
-    
-    for(var i = 0; i < elementInputs.lenth; i++) {
-        var input = elementInputs[i];
-        console.log(input.value, input.id, input.checked);
-    }
-
-    var elementInput = document.querySelectorAll('input[name="element"]');
-
-    if(selectedElementInput) {
+    var tags = document.querySelectorAll('input[name="element"]:checked');
+    for(var i = 0; i < elementResponse.length; i++) {
+        var tag = document.querySelectorAll(tags[i].value); 
+        var inputname = tags[i].value;
+        var numberelement = tag.length;
+        var elementResponse= (inputname + '_' + numberelement)
         
+        if (elementList === "P") {
+            elementResponse.textContent = elementResponse;
+        }
+        else if (elementList === "Div") {
+            elementResponse.textContent = elementResponse;
+        } 
+        else if (elementList === "Li") {
+            elementResponse.textContent = elementResponse;
+        }
+        else if (elementList === "Input") {
+            elementResponse.textContent = elementResponse;
+        }
     }
+    
 }
