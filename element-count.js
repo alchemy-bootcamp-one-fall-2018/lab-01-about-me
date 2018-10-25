757c146dfebac6d38508cf6913404155da40ce04
Dyanna Tolman <dyannast@gmail.com>
2018-10-24 17:45:31 -0700

    element and count appears in console.log

--- element-count.js
+++ element-count.js
@@ -4,9 +4,16 @@
 
 function tryQuerySelector() {
     var selectedElements = document.querySelectorAll('input[name="elements"]:checked');
-    //console.log(input.value);
+    console.log(selectedElements);
     for(var i = 0; i < selectedElements.length; i++) {
         var input = selectedElements[i];
         console.log(input.value);
+    
+        var elementCount = document.querySelectorAll('input.value');
+        console.log(elementCount.length);
     }
 }
+
+
+// var p = document.querySelectorAll('p');
+// console.log(p.length);
