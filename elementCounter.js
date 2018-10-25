16b3e7a1ebffc31dc8bc959593507d632082bd92
Teonna Zaragoza <teonna.zaragoza@gmail.com>
2018-10-24 17:46:41 -0700

    Entered in text response which appears on the page. Learned how to use a pre tag

--- elementCounter.js
+++ elementCounter.js
@@ -1,17 +1,29 @@
+var queryResponse = document.getElementById('query-results');
+
   function tryQuerySelector() {
       var inputCounter = document.querySelectorAll('input[name="counter"]:checked');
     
       for(var i=0; i < inputCounter.length; i++) {
-          
-          var input = document.querySelectorAll(inputCounter[i].value);
-          console.log(inputCounter[i].id, input.length);
+        var input = document.querySelectorAll(inputCounter[i].value);
+        console.log(inputCounter[i].id, input.length);
+
+        queryResponse.textContent = queryResponse.textContent + inputCounter[i].id + ' : ' + input.length + ' \r\n ';
+
       }
-    }  
+      
+     
+        
+ }
+ 
+
+        //   queryResponse.textContent = inputCounter[0].id + ': ' + input.length;
+
+        
+        
+
+        
+
+     
+     
 
     
-    //   var selectedCounterInput = document.querySelectorAll( 'input[name="counter"]:checked');
-      
-    //   var Counter = document.querySelectorAll('selectedCounterInput').length;
-
-    // }
-
