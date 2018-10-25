1567c1bd24ff0b4f24e51e4034dba5162b4f2721
Tyler Corbett <thetylercorbett@gmail.com>
2018-10-24 17:17:40 -0700

    can print answer to log and html

--- counter.js
+++ counter.js
@@ -1,9 +1,16 @@
+var elementCounterResponse = document.getElementById('element-counter-response');
+
+
 function clickedButton() {
     var tagsChosen = document.querySelectorAll('input[name="element-button"]:checked');    
     
     for (var i = 0; i < tagsChosen.length; i++) {
-        var tagCount = tagsChosen[i]
-        console.log(tagCount)
-    }
+        var tagCount = tagsChosen[i];
+        var divList = document.querySelectorAll(tagCount.value);
+        var numberDivs = divList.length;
 
+        var finalAnswer = (tagCount.value + '--' + numberDivs)
+        console.log(finalAnswer);
+        elementCounterResponse.textContent = finalAnswer;
+    }
 }
