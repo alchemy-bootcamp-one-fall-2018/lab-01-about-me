004f25496056946527cebea36fe0d8fa74869dda
Cheri A Portman <cheriportman@hotmail.com>
2018-10-25 11:03:02 -0700

    working element counter with assistance

--- ElementCount.js
+++ ElementCount.js
@@ -1,64 +1,31 @@
 /* eslint no-console: "off" */
 /* exported tryQuerySelector */
 'use strict';
+//I'm still working on this code; it works but I want to more fully understand this, will resubmit
+function tryQuerySelector() {
+    var selectedElementInput = document.querySelectorAll('input[name="element"]:checked');
+    console.log(selectedElementInput);
 
-
-
-function tryQuerySelector(){
-    //var elementInput = document.querySelectorAll('input[name="element"]');   //looks for checked elements
-    var selectedElementInput = document.querySelectorAll('input[name="element"]:checked');   //checks for number of checks per element
-    
-    for(var i = 0; i < selectedElementInput.length; i++) {        
+    for(var i = 0; i < selectedElementInput.length; i++) {
         var input = selectedElementInput[i];
-        console.log(input);
-        var elementList = document.querySelectorAll(input);
-        console.log(elementList);
-        var elementFreq = (elementList.length);
-        console.log(elementFreq);
+        console.log(input.value);
+        var tag = document.querySelectorAll(input.value);
+        console.log(tag.length);
+
+        if(input.value === 'p') {
+            document.getElementById('p-output').innerHTML = input.value + ' - ' + tag.length;
+        }
+        else if(input.value === 'section'){
+            document.getElementById('section-output').innerHTML = input.value + ' - ' + tag.length;
+        }
+        else if(input.value === 'form'){
+            document.getElementById('form-output').innerHTML = input.value + ' - ' + tag.length;
+        }
+        else if(input.value === 'input') {
+            document.getElementById('input-output').innerHTML = input.value + ' -' + tag.length;
+        }
     }
-      //  console.log(elementInput[i].value)
-      //  console.log(input.value, input.checked);
-    
 }
-    //console.log (selectedElementInput);
-
-    //for(var i = 0; i<selectedElementInput.length; i++) {
-
-    //}
-
-    //if(selectedElementInput) {
-      //  console.log(selectedElementInput.value);
-    //}else{
-      //  console.log(selectedElementInput + 'is zero');
-    //}
-    
-    
-
-//tryQuerySelector(selectedElementInput);
 
 
 
-
-
-//function tryQuerySelector() {
-//    var mealInputs = document.querySelectorAll('input[name="meal"]');
-//     for(var i = 0; i < mealInputs.length; i++) {
-//        var input = mealInputs[i];
-//        console.log(input.value, input.id, input.checked);
-//    }
-//     var selectedMealInput = document.querySelector('input[name="meal"]:checked');
-//     if(selectedMealInput) {
-//        console.log(selectedMealInput.value);
-//    }
-//    else {
-//        console.log('no meal selected');
-//  }
-//     console.log(xIsTheNumber);
-//}
-
-//var tags = document.querySelectorAll('input[name="tags"]:checked');
-//Create your response text as you are looping through the list of checked tags
-
-//Get the value of the checkbox by calling tags[i].value and store in a tag variable
-//Call document.querySelectorAll(tag) and add the tag and length of the returned query into your response text
-//Response text might look like:
