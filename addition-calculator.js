60ecdc69d29e0b6095949228f057f8c2ed694e21
Tyler Corbett <thetylercorbett@gmail.com>
2018-10-27 13:51:46 -0700

    finalized all changes

--- addition-calculator.js
+++ addition-calculator.js
@@ -7,16 +7,6 @@ function addNumbers() {
     var firstNumber = firstNumberInput.value;
     var secondNumberInput = elements.secondDigit;
     var secondNumber = secondNumberInput.value;
-
-    var submitButton = elements.submit;
-
     var answer = parseInt(firstNumber) + parseInt(secondNumber);
-
     randomNumberResponse.textContent = answer;
-    
-}
-
-
-function resetCalculator() {
-    console.log('rest works')
 }
