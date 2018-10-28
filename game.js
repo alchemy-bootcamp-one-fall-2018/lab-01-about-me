1f50a2662cb383c4ae366d074b5e4d3bdb5def20
Jennifer_Ramirez <jenramirez5@hotmail.com>
2018-10-27 20:59:01 -0700

    css changes and question 1 javascript

--- game.js
+++ game.js
@@ -1,12 +1,36 @@
-var aboutMeForm = document.getElementById("about-meform");
+var aboutMeForm = document.getElementById("about-me-form");
 var aboutMeResponse = document.getElementById("about-me-response");
 
 function checkAboutMe() {
     var elements = aboutMeForm.elements;
 
-    var favoriteInput = elements.favorite;
-    var likeClassmates = favoriteInput.value.toLowerCase();
+    var classInput = elements.class;
+    var likeClassmates = classInput.value;
 
+    var guessInput = elements.guess;
+    var age = guessInput.value;
+
+    var thingInput = elements.thing;
+    var favoriteThing = thingInput.value;
+
+    var teachInput = elements.teach;
+    var teachingClass = teachInput.value;
+
+    var placeInput = elements.place;
+    var favoritePlace = placeInput.value;
+
+    var correct = 0;
+
+    if(likeClassmates() === 'classmates') {
+        correct = correct ++;
+        classInput.classList.add('correct');
+        classInput.class.remove('incorrect');
+    } else if(likeClassmates !== '') {
+        classInput.classList.add('wrong');
+        classInput.classList.remove('correct';)
+    }
+
+    aboutMeReponse.textContent = 'Your score is' + correct + 'out of 5';
 }
 
 function submit {
