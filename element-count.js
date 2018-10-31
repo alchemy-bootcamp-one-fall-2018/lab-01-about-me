e2b6259c93f08392e7de118bc47d641d3be12dc0
Dyanna Tolman <dyannast@gmail.com>
2018-10-31 13:36:22 -0700

    added the element count in the DOM

--- element-count.js
+++ element-count.js
@@ -4,11 +4,13 @@
 
 function tryQuerySelector() {
     var selectedElements = document.querySelectorAll('input[name="elements"]:checked');
-    console.log(selectedElements);
+    var results = document.getElementById('results');
 
     for(var i = 0; i < selectedElements.length; i++) {
         var input = selectedElements[i].value;
-        console.log(document.querySelectorAll(input).length);
-               
+        console.log('input', input);
+        var elementCount = document.querySelectorAll(input).length;
+        console.log('element count', elementCount);
+        results.textContent += 'you have selected ' + input + ' ' + elementCount + ' ' + 'times' + ' ' ;
     }
 }
--- guess-number.js
+++ guess-number.js
@@ -1,4 +1,5 @@
 /* exported checkGuessNumber, resetGuessNumber */
+'use strict';
 
 var guessNumber = document.getElementById('guess-number');
 var guessNumberResponse = document.getElementById('guess-number-response');
@@ -10,16 +11,16 @@ function checkGuessNumber() {
     var elements = guessNumber.elements;
     var guess = elements.number.value;
     var submit = elements.guess;
-
+    //var guessAllowed = 4;
     if(guess === '7') {
         guessNumberResponse.textContent = 'You got it. You win!';
         submit.disabled = true;
     }
     else {
         guessCount = guessCount + 1;
-
+       // guessesAllowed = guessesAllowed--;
         if(guessCount < 3) {
-            guessNumberResponse.textContent = 'Try again!';
+            guessNumberResponse.textContent = 'Try again!' + guess + ' left';
         }
         else {
             guessNumberResponse.textContent = 'Game Over! Better luck next time!';
