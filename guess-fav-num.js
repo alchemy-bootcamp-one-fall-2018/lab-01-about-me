fec35432b2f4a6db2254940d1687b1c407cccf70
Jennifer_Ramirez <jenramirez5@hotmail.com>
2018-10-30 21:43:49 -0700

    add element counter game code in javascript

---
+++ guess-fav-num.js
@@ -0,0 +1,32 @@
+var guessNumberForm = document.getElementById('guess-number-form');
+var guessNumberResponse = document.getElementById('guess-number-response');
+
+var guessCount = 0;
+
+function checkGuessNumber() {
+    var elements = guessNumberForm.elements;
+    var guess = elements.number.value;
+    var submit = elements.guess;
+
+    if(guess == 5) {
+        guessNumberResponse.textContent = 'You are Correct!!!';
+        submit.disabled = true;
+    } else {
+        guessCount = guessCount + 1;
+
+        if(guessCount < 5) {
+            guessNumberResponse.textContent = 'Sorry that is wrong';
+        } else {
+            guessNumberResponse.textContent = 'You lose!';
+            submit.disabled = true;
+        }
+    }
+}
+
+function resetGuessNumber() {
+    var elements = guessNumberForm.elements;
+    var submit = elements.guess;
+    submit.disabled = false;
+    guessCount = 0;
+    guessNumberResponse.textContent = '';
+}
