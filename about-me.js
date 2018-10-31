1d896870e33c3bfd40c13e641ea5531ac0b5fe6c
Jennifer_Ramirez <jenramirez5@hotmail.com>
2018-10-30 21:29:40 -0700

    fixed about-me game questions and styled games

--- about-me.js
+++ about-me.js
@@ -9,7 +9,7 @@ function checkAboutMe() {
     var likeClassmates = classInput.value;
 
     var guessInput = elements.guess;
-    var age = guessInput.value;
+    var guessAge = guessInput.value;
 
     var thingInput = elements.thing;
     var favoriteThing = thingInput.value;
@@ -22,19 +22,72 @@ function checkAboutMe() {
 
     var correct = 0;
 
-    if(likeClassmates() === 'classmates') {
-        correct = correct ++;
+    if(likeClassmates.toLowerCase() === 'classmates') {
+        correct = correct + 1;
         classInput.classList.add('correct');
-        classInput.class.remove('incorrect');
+        classInput.classList.remove('wrong');
     } else if(likeClassmates !== '') {
         classInput.classList.add('wrong');
         classInput.classList.remove('correct');
     }
 
+    if(guessAge === '33') {
+        correct = correct + 1;
+        guessInput.classList.add('correct');
+        guessInput.classList.remove('wrong');
+    } else if(guessAge !== '') {
+        guessInput.classList.add('wrong');
+        guessInput.classList.remove('correct');
+    }
+
+    if(favoriteThing.toLowerCase() === 'food') {
+        correct = correct + 1;
+        thingInput.classList.add('correct');
+        thingInput.classList.remove('wrong');
+    } else if(likeClassmates !== '') {
+        thingInput.classList.add('wrong');
+        thingInput.classList.remove('correct');
+    }
+
+    if(teachingClass === 'no') {
+        correct = correct + 1;
+        teachInput.classList.add('correct');
+        teachInput.classList.remove('wrong');
+    } else if(guessAge !== '') {
+        teachInput.classList.add('wrong');
+        teachInput.classList.remove('correct');
+    }
+
+    if(favoritePlace === 'home') {
+        correct = correct + 1;
+        placeInput.classList.add('correct');
+        placeInput.classList.remove('wrong');
+    } else if(guessAge !== '') {
+        placeInput.classList.add('wrong');
+        placeInput.classList.remove('correct');
+    }
+
     aboutMeResponse.textContent = 'Your score is' + correct + 'out of 5';
 }
-/*
-function submit {
-    var newVar = document.getElementById("submitCheckbox");
+
+function resetAboutMe() {
+    var elements = aboutMeForm.elements;
+    var classInput = elements.class;
+    var guessInput = elements.guess;
+    var thingInput = elements.thing;
+    var teachInput = elements.teach;
+    var placeInput = elements.place;
+
+    classInput.classList.remove('correct');
+    classInput.classList.remove('wrong');
+    guessInput.classList.remove('correct');
+    guessInput.classList.remove('wrong');
+    thingInput.classList.remove('correct');
+    thingInput.classList.remove('wrong');
+    teachInput.classList.remove('correct');
+    teachInput.classList.remove('wrong');
+    placeInput.classList.remove('correct');
+    placeInput.classList.remove('wrong');
+
+    aboutMeResponse.textContent = '';
 }
-console.log ("Help me");*/
